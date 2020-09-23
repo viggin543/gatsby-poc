019f0d2c4d14f82a16d588672cf77ec2ede0f7e8
Domrev Igor <domrevigor@gmail.com>
2020-09-23 10:56:58 +0300

    adding netlify login

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,45 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect } from "react"
+import netlifyIdentity from "netlify-identity-widget"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  useEffect(() => {
+    netlifyIdentity.init()
+  }, []);
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
+      <div
+        style={{
+          display: 'flex',
+          alignItems: "center",
+          justifyContent: "space-between",
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`
+        }}
+      >
+        <div data-netlify-identity-menu></div>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  )
+}
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
